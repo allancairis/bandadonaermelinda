11ea2fbf3d2aa085ba8fc9acfebf227e0458a818
Allan Bittencourt Cairis <allan.cairis@softplan.com.br>
2023-09-20 15:37:12 -0300

    Criando projeto

---
+++ BandaDonaErmelinda/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BandaDonaErmelinda.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
